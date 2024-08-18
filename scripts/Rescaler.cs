9e5ed80eb925cf6be6d18e698aaf645d7d50b722
Benjamin Rendell <benjamin.rendell@students.mq.edu.au>
2024-08-18 22:29:13 +1000

    Force applied during rescaling.

--- scripts/Rescaler.cs
+++ scripts/Rescaler.cs
@@ -1,12 +1,14 @@
 using Godot;
 
-public partial class Rescaler : Node2D
+public partial class Rescaler : RigidBody2D
 {
 	[Export] public Vector2 originalScale;
 	[Export] public Vector2 newScale;
 	[Export] public float step = 1;
+	[Export] public float impulsePower;
 	private float t = 0.0f;
 	private bool rescaling = false;
+	private bool changeScale = false;
 	Node2D child;
     public override void _Ready()
     {
@@ -17,8 +19,9 @@ public partial class Rescaler : Node2D
     public override void _PhysicsProcess(double delta)
     {
 		if(Input.IsActionJustPressed("shoot")){
-			rescaling = !rescaling;
+			changeScale = !changeScale;
 			t = 1-t;
+			rescaling = true;
 		}
         Resize(newScale, delta);
     }
@@ -26,11 +29,47 @@ public partial class Rescaler : Node2D
 	public void Resize(Vector2 targetScale, double delta){
 		t += (float)delta * step;								//Increment `t` based on the current step and delta
 		t = Mathf.Clamp(t, 0, 1);				//Clamp `t` between 0 and 1
-		if(rescaling){
+		if(changeScale){
 			child.Scale = originalScale.Lerp(targetScale, t);
 		}else{
 			child.Scale = targetScale.Lerp(originalScale, t);
 		}
-		//GD.Print(Scale);
+		if(child.Scale == targetScale || child.Scale == originalScale){
+			rescaling = false;
+		}
+		GD.Print(rescaling);
+	}
+
+	//detects collisions
+	void OnCollisionEnter(Node2D other){
+		if(rescaling){
+			AddForce(other);
+		}
+	}
+
+	void AddForce(Node2D other){
+        if (other is RigidBody2D rb2d)
+        {
+            Vector2 direction = other.Position - this.Position;
+			rb2d.ApplyCentralImpulse(direction * impulsePower);
+        }
+		else if (other is CharacterBody2D character){
+			// Calculate direction vector
+			Vector2 direction = character.Position - this.Position;
+			// Apply a force along the direction vector
+			Vector2 forceDirection = direction.Normalized() * impulsePower * 100f;
+			// Apply the combined force to the CharacterBody2D
+			character.Velocity += forceDirection;
+		}
+        
+	}
+
+	void AddForceOld(Node2D other){
+        if (other is not RigidBody2D rb2d)
+        {
+            return;
+        }
+        Vector2 direction = other.Position - this.Position;
+		rb2d.ApplyCentralImpulse(direction * impulsePower);
 	}
 }
